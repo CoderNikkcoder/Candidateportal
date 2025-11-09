a9a646f4fcdb55cfba16b59f486bb77c90ba72f5
CoderNikkcoder <159905764+CoderNikkcoder@users.noreply.github.com>
2025-11-09 19:49:31 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-// src/App.js
+
 import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CandidateForm from "./pages/CandidateForm.js";
